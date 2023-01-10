aa0a308b70d26ce7f465ed14e0e98bf7adcb85d1
Sai Charan Reddy <saicharanreddy1999.ct@gmail.com>
2023-01-10 20:30:32 +0530

    parse

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -30,7 +30,7 @@ const cartSlice = createSlice({
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
     shippingInfo: localStorage.getItem('shippingInfo')
-      ? JSON.parse(`${localStorage.getItem('shippingInfo')}`)
+      ? JSON.parse(`'${localStorage.getItem("shippingInfo")}'`)
       : {
           address: '',
           city: '',
